f79dbbb1a2a4a0604530e9e3805179e959dc3e5d
yashu159357 <yashu159357@gmail.com>
2020-09-20 18:08:11 +0530

    Updates

---
+++ precache-manifest.2d0f4e4962290f00513a01d82d71af19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2d64489751452ed4255374f4dace39",
+    "url": "/focus/index.html"
+  },
+  {
+    "revision": "e1c58db5aa2cfed99853",
+    "url": "/focus/static/css/main.4204c910.chunk.css"
+  },
+  {
+    "revision": "edfa1df5e7b757f6b1f0",
+    "url": "/focus/static/js/2.de53f4f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/focus/static/js/2.de53f4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c58db5aa2cfed99853",
+    "url": "/focus/static/js/main.ba12fceb.chunk.js"
+  },
+  {
+    "revision": "26d6926b98d14ff5e45c",
+    "url": "/focus/static/js/runtime-main.d24edf5f.js"
+  }
+]);
